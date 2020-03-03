8d28c31d806c19a798fa0a0fae71c0ede805bc28
chowdigital <sean@chowdigital.com>
2020-03-03 09:06:43 +0000

    footer responsive

--- footer.php
+++ footer.php
@@ -25,45 +25,55 @@ jQuery(document).ready(function($) {
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-title">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/cluettlogo.svg"/>
-					<?php bloginfo( 'name' ); ?></a>
-		</div>
+		
+		<div class="footer-flex">
 
-	<div class="contact-form">
-		<h3 class="light-text">Get in touch</h3>
-		<?php echo do_shortcode( '[contact-form-7 id="71" title="START FORM"]' ); ?>
+			<div class="contact-form">
+				<h3 class="light-text">Get in touch</h3>
+				<?php echo do_shortcode( '[contact-form-7 id="71" title="START FORM"]' ); ?>
 
-
-	</div>
-		<div class="site-info">
-			<div class="site-info-column">
-				<h5>Title A</h5>
-				<a href="link">title A</a>
-				<a href="link">title A</a>
-				<a href="link">title A</a>
 			</div>
-			<div class="site-info-column">
-				<h5>Title B</h5>
-				<a href="link">title B</a>
-				<a href="link">title B</a>
-				<a href="link">title B</a>
-			</div>
+			
 
-			<div class="site-info-column">
-				<h5>Title C</h5>
-				<a href="link">title C</a>
-				<a href="link">title C</a>
-				<a href="link">title C</a>
-			</div>
+			<div class="site-info">
+				<div class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/cluettlogo.svg"/>
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</div>
+				<div class="site-info-wrap">
+				<div class="site-info-column">
+					<h5>Title A</h5>
+					<a href="link">title A</a>
+					<a href="link">title A</a>
+					<a href="link">title A</a>
+				</div>
+				<div class="site-info-column">
+					<h5>Title B</h5>
+					<a href="link">title B</a>
+					<a href="link">title B</a>
+					<a href="link">title B</a>
+				</div>
 
-		</div><!-- .site-info -->
+				<div class="site-info-column">
+					<h5>Title C</h5>
+					<a href="link">title C</a>
+					<a href="link">title C</a>
+					<a href="link">title C</a>
+				</div>
+				</div>
+
+			</div><!-- .site-info -->
+
+			<div class="break"></div>
+
+		</div><!-- footer-flex-->
 		<div class="social-links">
-		<a href="#facbook">
+		<a href="https://en-gb.facebook.com/cluettcarpentry">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/facebook.svg"/>
 		</a>
-		<a href="#instagram">
+		<a href="https://www.instagram.com/CluettCarpentry/?hl=en">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/instagram.svg"/>
 		</a>
 
@@ -78,3 +88,4 @@ jQuery(document).ready(function($) {
 
 </body>
 </html>
+© 2020 GitHub, Inc.
--- functions.php
+++ functions.php
@@ -122,7 +122,7 @@ add_action( 'widgets_init', 'cluett_3_3_widgets_init' );
 function cluett_3_3_scripts() {
 	wp_enqueue_style( 'cluett-3-3-style', get_stylesheet_uri() );
 
-	wp_enqueue_style( 'cluett-style', get_template_directory_uri() . '/css/cluett.min.css?v=1.0', array(), '1.0', 'all' );
+	wp_enqueue_style( 'cluett-style', get_template_directory_uri() . '/css/cluett.css?v=1.0', array(), '1.0', 'all' );
 
 	wp_enqueue_style( 'typekit', 'https://use.typekit.net/ivs4wxb.css', array(), '1.0', 'all' );
 
